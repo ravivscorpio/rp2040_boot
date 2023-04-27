ee4f11122ff565009faec0ef93693bdbd80a06ea
Raviv <raviv.kiron@scorpio.space>
2023-04-27 11:12:17 +0300

    boot works with go serial-flash app with power cycle within 5sec

---
+++ build/CMakeFiles/bootloader.dir/C_/Users/raviv/Downloads/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.obj.d
@@ -0,0 +1,37 @@
+CMakeFiles\bootloader.dir\C_\Users\raviv\Downloads\pico-sdk\src\rp2_common\pico_standard_link\binary_info.c.obj: \
+ C:\Users\raviv\Downloads\pico-sdk\src\rp2_common\pico_standard_link\binary_info.c \
+ C:\Users\raviv\Downloads\pico-sdk\src\common\pico_binary_info\include/pico/binary_info.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\common\pico_binary_info\include/pico/binary_info/defs.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\common\pico_binary_info\include/pico/binary_info/structure.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\lib\gcc\arm-none-eabi\12.2.1\include\stdint.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\arm-none-eabi\include\stdint.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\arm-none-eabi\include\machine\_default_types.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\arm-none-eabi\include\sys\features.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\arm-none-eabi\include\_newlib_version.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\arm-none-eabi\include\sys\_intsup.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\arm-none-eabi\include\sys\_stdint.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\common\pico_binary_info\include/pico/binary_info/code.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\common\pico_base\include/pico.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\common\pico_base\include/pico/types.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\common\pico_base\include/pico/assert.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\lib\gcc\arm-none-eabi\12.2.1\include\stdbool.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\arm-none-eabi\include\assert.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\arm-none-eabi\include\_ansi.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\arm-none-eabi\include\newlib.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\arm-none-eabi\include\sys\config.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\arm-none-eabi\include\machine\ieeefp.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\lib\gcc\arm-none-eabi\12.2.1\include\stddef.h \
+ C:\Users\raviv\Documents\projects\pico-examples\rp2040-serial-bootloader-main\build\generated\pico_base/pico/version.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\common\pico_base\include/pico/config.h \
+ C:\Users\raviv\Documents\projects\pico-examples\rp2040-serial-bootloader-main\build\generated\pico_base/pico/config_autogen.h \
+ C:/Users/raviv/Downloads/pico-sdk/src/boards/include/boards/pico.h \
+ C:/Users/raviv/Downloads/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\rp2_common\pico_platform\include/pico/platform.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\rp2040\hardware_regs\include/hardware/platform_defs.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/addressmap.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/sio.h \
+ c:\progra~2\armgnu~1\12dbab~1.2re\arm-none-eabi\include\sys\cdefs.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\common\pico_base\include/pico/types.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\common\pico_base\include/pico/error.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\rp2_common\boot_stage2\include/boot_stage2/config.h \
+ C:\Users\raviv\Downloads\pico-sdk\src\common\pico_base\include/pico/config.h
